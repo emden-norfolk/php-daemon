21eb2905ec9af4783c760f40f981c44300545018
Damien Bezborodov <dbezborodovrp@users.noreply.github.com>
2017-01-17 16:21:39 +1030

    Add daemon.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # php-daemon
 PHP Daemoniser
+
+Usage:
+
+/var/www/html/daemon/phpdaemon \
+	/var/www/html/daemon/example.php \
+	/var/www/html/daemon/example.0.pid \
+	/var/www/html/daemon/example.log 0
+
---
+++ example.php
@@ -0,0 +1,14 @@
+<?php
+
+function main(array $argv, $argc, $wrkn) {
+	while (running()) {
+		// Log to file.
+		trigger_error("Hello, world (Worker number: $wrkn)", E_USER_NOTICE);
+
+		// Note that echo will break the daemon because STDOUT is closed.
+		// echo "Bad goy!"; <-- Do NOT do this!
+
+		// Do work here.
+		sleep(300);
+	}
+}
